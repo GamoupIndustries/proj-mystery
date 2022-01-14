fcff46bdd576802b54130825d2fac19e140c8426
TheSixLegendsOG <75628007+GamoupIndustries@users.noreply.github.com>
2022-01-14 11:12:07 -0500

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,15 @@
+class Title extends Phaser.Scene {
+  constructor() {
+    super("title");
+  }
+  preload() {
+    this.load.image("player", "assets/images/player.png");
+  }
+  create() {
+    var text = this.add.text(20, 20, "Loading Game...");
+    var player = this.add.image(270, 270, "player");
+  }
+  update() {
+
+  }
+}
